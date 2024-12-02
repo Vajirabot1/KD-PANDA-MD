efd72d04c0a76fdf067392e7027ddf30c243516a
Vajirabot1 <Vajirabot1@gmail.com>
2024-12-03 00:35:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "VAJIRA-MD=bGJmzSpK#yN9qImjKuF6cwQefRYNFBo5FJDkM0ElEMZAovsprBNs",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
