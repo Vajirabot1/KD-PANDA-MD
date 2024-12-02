9ae285b71739bbf94f0439ac41cf0ebff70e04cf
Vajirabot1 <Vajirabot1@gmail.com>
2024-12-03 00:33:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,18 +47,21 @@ const Crypto = require('crypto')
 const path = require('path')
 const prefix = config.PREFIX
 
-const ownerNumber = ['923096287432']
+const ownerNumber = ['94776114551']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID
-const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
-filer.download((err, data) => {
-if(err) throw err
-fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-console.log("SESSION DOWNLOADED COMPLETED ✅")
-})})}
+    if (config.SESSION_ID) {
+      const sessdata = config.SESSION_ID.replace("VAJIRA-MD=", "")
+      const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+      filer.download((err, data) => {
+        if (err) throw err
+        fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+          console.log("Session download completed !!")
+        })
+      })
+    }
+  }
 
 const express = require("express");
 const app = express();
