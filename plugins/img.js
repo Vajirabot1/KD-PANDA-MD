bdd638630b598841ae18b2c663e1066107e0627c
Vajirabot1 <Vajirabot1@gmail.com>
2024-12-03 07:48:56 +0530

    Update img.js

--- plugins/img.js
+++ plugins/img.js
@@ -66,7 +66,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
  *Enjoy these images! 👾*
 
-> 🌈*SILENT-SOBX-MD BOT PLUGINS*🏝️`
+> 👿☠️_KD_PANTA_00_☠️👿`
 }, { quoted: mek });
 }
 
